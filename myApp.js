3d3b56f5e5c0de816d40a17d1dedd8c747b386b7
Vinicius Leal Alves <59040683+lealalves@users.noreply.github.com>
2022-11-01 17:55:08 +0000

    added middleware for css file

--- myApp.js
+++ myApp.js
@@ -2,6 +2,8 @@ let express = require('express');
 let app = express();
 
 
+app.use('/public', express.static(__dirname + '/public'))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 })
